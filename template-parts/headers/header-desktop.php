9866473dabeca627420649bcd22576943b336a18
khizarhayatt <113554002+khizarhayatt@users.noreply.github.com>
2023-09-26 16:39:08 -0700

    w

--- template-parts/headers/header-desktop.php
+++ template-parts/headers/header-desktop.php
@@ -1,7 +1,7 @@
 <!-- Small header. -->
-<div class="small-header container">
+<div class="small-header container mt-2 ">
     <div class="row">
-        <div class="col md-6 col-12">
+        <div class="col-md-6 col-12">
             <?php 
             wp_nav_menu(
                 array(
@@ -12,7 +12,33 @@
              )
             ?>
         </div>
-        <div class="col md-6 col-12"></div>
+        <div class="col-md-6 col-12">
+            <ul class="social-icons">
+                <li class="social link"><a href="#"> <i class="fa-brands fa-facebook"></i></a></li>
+                <li class="social link"><a href="#"> <i class="fa-brands fa-twitter"></i></a></li>
+                <li class="social link"><a href="#"> <i class="fa-brands fa-instagram"></i></a></li>
+                <li class="social link"><a href="#"> <i class="fa-brands fa-youtube"></i></a></li>
+            </ul>
+        </div>
     </div>
+</div>
 
+<!-- Main header. -->
+<div class="main-header container">
+    <div class="row">
+        <div class="site-branding col-md-4">
+            <a href="javascript:void(0)" id="desktop-menu-toggle" class="mega-menu-toggle">
+                <i class="fas fa-bars"></i>
+            </a>
+            <?php
+            if(has_custom_logo()):
+                the_custom_logo();
+            else:
+                echo  bloginfo('name') ;
+            endif;    
+                ?>
+        </div>
+        <div class="search-col col-md-4"></div>
+        <div class="buttons-col col-md-4"></div>
+    </div>
 </div>
